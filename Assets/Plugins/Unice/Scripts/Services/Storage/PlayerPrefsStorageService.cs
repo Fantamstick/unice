7cc6cd8a5d716698d8fa0319f754439af3981c09
Jonathan Bro <jonathan.b@fantamstick.jp>
2021-02-04 11:39:07 +0900

    Add storage service

---
+++ Assets/Plugins/Unice/Scripts/Services/Storage/PlayerPrefsStorageService.cs
@@ -0,0 +1,38 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "player prefs storage service", menuName = "App/Services/PlayerPrefsStorage")]
+public class PlayerPrefsStorageService : StorageService {
+    /// <summary>
+    /// Save string data.
+    /// </summary>
+    /// <param name="key">data key.</param>
+    /// <param name="data">data</param>
+    /// <returns>Did overwrite value</returns>
+    public override async UniTask<bool> SaveAsync(string key, string data) {
+        bool hasKey = PlayerPrefs.HasKey(key);
+        PlayerPrefs.SetString(key, data);
+
+        return await UniTask.FromResult(hasKey);
+    }
+
+    /// <summary>
+    /// Load string data.
+    /// </summary>
+    /// <param name="key">storage key</param>
+    /// <returns>Value from key. Empty string if value does not exist.</returns>
+    public override async UniTask<string> LoadAsync(string key) {
+        string data = PlayerPrefs.GetString(key);
+        return await UniTask.FromResult(data);
+    }
+
+    /// <summary>
+    /// Delete data.
+    /// </summary>
+    /// <param name="key">data key.</param>
+    public override UniTask DeleteAsync(string key) {
+        PlayerPrefs.DeleteKey(key);
+
+        return UniTask.CompletedTask;
+    }
+}
---
+++ Assets/Plugins/Unice/Scripts/Services/Storage/StorageService.cs
@@ -0,0 +1,23 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public abstract class StorageService : ScriptableObject {
+    /// <summary>
+    /// Save string data.
+    /// </summary>
+    /// <param name="key">data key.</param>
+    /// <param name="data">data</param>
+    /// <returns>Did overwrite value</returns>
+    public abstract UniTask<bool> SaveAsync(string key, string data);
+
+    /// <summary>
+    /// Load string data.
+    /// </summary>
+    public abstract UniTask<string> LoadAsync(string key);
+
+    /// <summary>
+    /// Delete data.
+    /// </summary>
+    /// <param name="key">data key.</param>
+    public abstract UniTask DeleteAsync(string key);
+}
